5901391a4a744ab4dc9a6a6c8ded8caaa02bfb4d
Harold <harold.malecot@gmail.com>
2020-02-10 22:51:32 +0100

    before working on NINO TODO

--- app/src/main/java/com/cleanup/todoc/MainApplication.java
+++ app/src/main/java/com/cleanup/todoc/MainApplication.java
@@ -6,6 +6,8 @@ import com.cleanup.todoc.data.dao.ProjectDao;
 import com.cleanup.todoc.data.dataBase.AppDatabase;
 import com.cleanup.todoc.data.model.Project;
 
+import java.util.List;
+
 public class MainApplication extends Application {
 
     private static Application sApplication;
@@ -24,8 +26,11 @@ public class MainApplication extends Application {
 
         ProjectDao projectDao = AppDatabase.getInstance().projectDao();
 
+        // TODO : Fixer la creation des projet quand l'application se relance après un kill voulu
+        // TODO : les projets s'accumulent à chaque fois
         projectDao.insertProject(new Project("Projet Tartampion"));
         projectDao.insertProject(new Project("Projet Lucidia"));
         projectDao.insertProject(new Project("Projet Circus"));
+
     }
 }
