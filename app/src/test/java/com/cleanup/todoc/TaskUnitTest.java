a11524acff731e425214eafde1459d9ce10484f7
Harold <harold.malecot@gmail.com>
2020-02-13 13:02:15 +0100

    Work On Unit test AZ comparator

--- app/src/test/java/com/cleanup/todoc/TaskUnitTest.java
+++ app/src/test/java/com/cleanup/todoc/TaskUnitTest.java
@@ -5,11 +5,11 @@ import androidx.lifecycle.MutableLiveData;
 
 import com.cleanup.todoc.data.model.Project;
 import com.cleanup.todoc.data.model.Task;
-import com.cleanup.todoc.data.repository.ProjectRepository;
 import com.cleanup.todoc.data.repository.ProjectRoomRepository;
-import com.cleanup.todoc.data.repository.TaskRepository;
 import com.cleanup.todoc.data.repository.TaskRoomRepository;
+import com.cleanup.todoc.model.TaskCellModelUi;
 import com.cleanup.todoc.model.TasksModelUi;
+import com.cleanup.todoc.ui.viewmodel.SortMethod;
 import com.cleanup.todoc.ui.viewmodel.TaskViewModel;
 
 import org.junit.Before;
@@ -27,7 +27,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
 
 /**
  * Unit tests for tasks
@@ -41,10 +40,10 @@ public class TaskUnitTest {
     public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
 
     @Mock
-    private ProjectRoomRepository projectRepository;
+    private ProjectRoomRepository projectRoomRepository;
 
     @Mock
-    private TaskRoomRepository taskRepository;
+    private TaskRoomRepository taskRoomRepository;
 
     private TaskViewModel viewModel;
 
@@ -56,12 +55,10 @@ public class TaskUnitTest {
         projectsLiveData = new MutableLiveData<>();
         tasksLiveData = new MutableLiveData<>();
 
-        given(projectRepository.getProjectListLiveData()).willReturn(projectsLiveData);
-        given(taskRepository.getTaskListLiveData()).willReturn(tasksLiveData);
+        given(projectRoomRepository.getProjectListLiveData()).willReturn(projectsLiveData);
+        given(taskRoomRepository.getTaskListLiveData()).willReturn(tasksLiveData);
 
-        // viewModel = mock(TaskViewModel.class);
-
-        viewModel = new TaskViewModel(projectRepository, taskRepository);
+        viewModel = new TaskViewModel(projectRoomRepository, taskRoomRepository);
     }
 
     @Test
@@ -72,7 +69,7 @@ public class TaskUnitTest {
         projectsLiveData.setValue(projects);
 
         List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task(0, "Nettoyer Android.support c naze", new Date().getTime()));
+        tasks.add(new Task(0, "task 1", new Date().getTime()));
         tasksLiveData.setValue(tasks);
 
         // When
@@ -83,40 +80,45 @@ public class TaskUnitTest {
         assertEquals(0, tasksModelUi.getEmptyTaskNameErrorStringRes());
         assertNotNull(tasksModelUi.getTaskCellModels());
         assertEquals(1, tasksModelUi.getTaskCellModels().size());
-        assertEquals("Nettoyer Android.support c naze", tasksModelUi.getTaskCellModels().get(0).getName());
+        assertEquals("task 1", tasksModelUi.getTaskCellModels().get(0).getName());
+    }
+
+    @Test
+    public void test_az_comparator() throws InterruptedException {
+        // Given
+        List<Project> projects = new ArrayList<>();
+        projects.add(new Project("First project"));
+        projectsLiveData.setValue(projects);
+
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task(0, "task 1", new Date().getTime()));
+        tasks.add(new Task(1, "task 2", new Date().getTime()));
+        tasks.add(new Task(2, "task 3", new Date().getTime()));
+        tasksLiveData.setValue(tasks);
+
+        // When
+        // viewModel.setSortMethod(SortMethod.ALPHABETICAL);
+        TasksModelUi tasksModelUi = LiveDataTestUtil.getOrAwaitValue(viewModel.getTaskModelUiMediatorLiveData());
+        // List<TasksModelUi> tasksModelUiList = LiveDataTestUtil.getOrAwaitValueList(viewModel.getTaskModelUiMediatorLiveData());
+        // List<TaskCellModelUi> taskCellModelUiList = LiveDataTestUtil.getOrAwaitValueListCell(viewModel.getTaskModelUiMediatorLiveData());
+
+        List<TaskCellModelUi> result = tasksModelUi.getTaskCellModels();
+        result.add(tasksModelUi.getTaskCellModels().get(0));
+        result.add(tasksModelUi.getTaskCellModels().get(1));
+        result.add(tasksModelUi.getTaskCellModels().get(2));
+
+        // Then
+        assertFalse(tasksModelUi.isEmptyStateDisplayed());
+        assertEquals(0, tasksModelUi.getEmptyTaskNameErrorStringRes());
+        assertNotNull(tasksModelUi.getTaskCellModels());
+        assertEquals(1, tasksModelUi.getTaskCellModels().size());
+        assertEquals("task 1", tasksModelUi.getTaskCellModels().get(0).getName());
+        assertEquals("task 1", result.get(0).getName());
+        assertEquals("task 2", result.get(1).getName());
+        assertEquals("task 3", result.get(2).getName());
     }
 
     /*
-    @Test
-    public void test_projects() {
-
-        final TaskCellModelUi task1 = new TaskCellModelUi(1, 1, "task 1", new Date().getTime());
-        final TaskCellModelUi task2 = new TaskCellModelUi(2, 2, "task 2", new Date().getTime());
-        final TaskCellModelUi task3 = new TaskCellModelUi(3, 3, "task 3", new Date().getTime());
-        final TaskCellModelUi task4 = new TaskCellModelUi(4, 4, "task 4", new Date().getTime());
-
-        assertEquals("Projet Tartampion", task1.getProject().getName());
-        assertEquals("Projet Lucidia", task2.getProject().getName());
-        assertEquals("Projet Circus", task3.getProject().getName());
-        assertNull(task4.getProject());
-    }
-
-    @Test
-    public void test_az_comparator() {
-        final TaskCellModelUi task1 = new TaskCellModelUi(1, 1, "aaa", 123);
-        final TaskCellModelUi task2 = new TaskCellModelUi(2, 2, "zzz", 124);
-        final TaskCellModelUi task3 = new TaskCellModelUi(3, 3, "hhh", 125);
-
-        final ArrayList<TaskCellModelUi> tasks = new ArrayList<>();
-        tasks.add(task1);
-        tasks.add(task2);
-        tasks.add(task3);
-        Collections.sort(tasks, new TaskCellModelUi.TaskAZComparator());
-
-        assertSame(tasks.get(0), task1);
-        assertSame(tasks.get(1), task3);
-        assertSame(tasks.get(2), task2);
-    }
 
     @Test
     public void test_za_comparator() {
